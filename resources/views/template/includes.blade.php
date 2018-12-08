d7a827c379e5c93580dd33459a04d78f6f53673e
Zachary Falkner <falkzach@gmail.com>
2018-12-08 14:37:22 -0700

    migrate from bower to yarn

--- resources/views/template/includes.blade.php
+++ resources/views/template/includes.blade.php
@@ -1,7 +1,7 @@
 <!-- Bootstrap core CSS -->
-<link href="{!! asset("bower_components/bootstrap/dist/css/bootstrap.min.css") !!}" rel="stylesheet">
-<link href="{!! asset("bower_components/font-awesome/css/font-awesome.min.css") !!}" rel="stylesheet">
-<link href="{!! asset("bower_components/bootstrap-social/bootstrap-social.css") !!}" rel="stylesheet">
+<link href="{!! asset("resources/bootstrap/dist/css/bootstrap.min.css") !!}" rel="stylesheet">
+<link href="{!! asset("resources/font-awesome/css/font-awesome.min.css") !!}" rel="stylesheet">
+<link href="{!! asset("resources/bootstrap-social/bootstrap-social.css") !!}" rel="stylesheet">
 
 <!-- Custom styles for this template -->
 <link href="{!! asset("css/cover.css") !!}" rel="stylesheet">
--- resources/views/template/javascript.blade.php
+++ resources/views/template/javascript.blade.php
@@ -1,7 +1,6 @@
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="{!! asset("bower_components/jquery/dist/jquery.min.js") !!}"></script>
-<script src="{!! asset("bower_components/tether/dist/js/tether.min.js") !!}"></script>
-<script src="{!! asset("bower_components/bootstrap/dist/js/bootstrap.min.js") !!}"></script>
+<script src="{!! asset("resources/jquery/dist/jquery.min.js") !!}"></script>
+<script src="{!! asset("resources/bootstrap/dist/js/bootstrap.min.js") !!}"></script>
 @yield('javascript')
