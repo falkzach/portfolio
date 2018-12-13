fe5a7b05b27f83b95765100e3c5c47a2f6558213
Zachary Falkner <falkzach@gmail.com>
2018-12-13 15:15:39 -0700

    fix navbar, add source link to footer

--- resources/views/template/foot.blade.php
+++ resources/views/template/foot.blade.php
@@ -1,3 +1,5 @@
 <div class="mastfoot">
-
+    <a href="https://github.com/falkzach/portfolio" target="_blank">
+        Portfolio Source
+    </a>
 </div>
--- resources/views/template/nav.blade.php
+++ resources/views/template/nav.blade.php
@@ -1,12 +1,15 @@
-<div class="masthead clearfix">
-    <div class="inner">
-        <h3 class="masthead-brand">{{ config('portfolio.handle') }}</h3>
-        <nav class="navbar navbar-expand-lg nav-masthead">
-            <a class="nav-link {{ Request::route()->getName() === 'portfolio_index' ? 'active' : null }}" href="{{ route('portfolio_index') }}">Home</a>
-            <a class="nav-link {{ Request::route()->getName() === 'projects' ? 'active' : null }}" href="{{ route('projects') }}">Graduate Projects</a>
-            <a class="nav-link {{ Request::route()->getName() === 'cv' ? 'active' : null }}" href="{{ route('cv') }}">Graduate CV</a>
-            <a class="nav-link {{ Request::route()->getName() === 'outdoors' ? 'active' : null }}" href="{{ route('outdoors') }}">Outdoors</a>
-            <a class="nav-link {{ Request::route()->getName() === 'contact' ? 'active' : null }}" href="{{ route('contact') }}">Contact</a>
-        </nav>
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark nav-masthead">
+    <a class="navbar-brand" href="#"><h3>{{ config('portfolio.handle') }}</h3></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active"><a class="nav-link {{ Request::route()->getName() === 'portfolio_index' ? 'active' : null }}" href="{{ route('portfolio_index') }}">Home</a></li>
+            <li class="nav-item active"><a class="nav-link {{ Request::route()->getName() === 'projects' ? 'active' : null }}" href="{{ route('projects') }}">Graduate Projects</a></li>
+            <li class="nav-item active"><a class="nav-link {{ Request::route()->getName() === 'cv' ? 'active' : null }}" href="{{ route('cv') }}">Graduate CV</a></li>
+            <li class="nav-item active"><a class="nav-link {{ Request::route()->getName() === 'outdoors' ? 'active' : null }}" href="{{ route('outdoors') }}">Outdoors</a></li>
+            <li class="nav-item active"><a class="nav-link {{ Request::route()->getName() === 'contact' ? 'active' : null }}" href="{{ route('contact') }}">Contact</a></li>
+        </ul>
     </div>
-</div>
+</nav>
